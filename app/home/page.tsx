9f178dbe12a86c043414c6bf42ecbc933c085d9b
chyun7114 <chyun7114@naver.com>
2025-09-26 18:10:20 +0900

    style: [#16] 메인 홈 위쪽 배너 구현

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/shared/ui/header/header";
+import MainBanner from "@/features/home/main-banner";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <MainBanner />
+    </>
+  );
+}
---
+++ src/features/home/main-banner.tsx
@@ -0,0 +1,32 @@
+export default function MainBanner() {
+  return (
+    <div className="w-full mx-auto mt-[70px] flex justify-center gap-[30px]">
+      <div className="w-[585px] h-[275px] px-[30px] py-[30px] bg-[#0ABF00] rounded-[18px]">
+        <div className="flex gap-[153px]">
+          <div className="w-[201px] flex flex-col gap-[12px]">
+            <p className="text-neutral-50 text-[28px] font-bold leading-10">
+              새로운 직관메이트
+              <br />
+              모집글 작성하기
+            </p>
+            <p className="text-[#ECECEE] text-base font-normal">직관 메이트를 모집해보세요</p>
+          </div>
+          <img src="/icons/post-icon.svg" className="mt-[35px]" />
+        </div>
+      </div>
+      <div className="w-[585px] h-[275px] px-[30px] py-[30px] bg-[#FF6D00] rounded-[18px]">
+        <div className="flex justify-between">
+          <div className="w-[201px] flex flex-col gap-[12px]">
+            <p className="text-neutral-50 text-[28px] font-bold leading-10">
+              직관 꿀팁
+              <br />
+              백과사전!
+            </p>
+            <p className="text-[#ECECEE] text-base font-normal">직관이 처음이신가요?</p>
+          </div>
+          <img src="/icons/dictionary.svg" className="mt-[35px]" />
+        </div>
+      </div>
+    </div>
+  );
+}

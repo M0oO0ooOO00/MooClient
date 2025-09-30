39ed64546a5fbd73d11b0ec3f9ba3ec9d4afc472
chyun7114 <chyun7114@naver.com>
2025-09-30 14:49:50 +0900

    style : [#16] 디자인 시스템 적용

--- src/features/home/mate-item.tsx
+++ src/features/home/mate-item.tsx
@@ -13,10 +13,10 @@ export default function MateItem({
 
   return (
     <div className={`px-[40px] py-[20px] bg-white ${roundedClass}`}>
-      <div className="flex justify-between text-[16px]">
+      <div className="flex justify-between b02-m">
         <div className="flex items-center gap-10">
-          <p className="w-[120px] truncate">{match}</p>
-          <p className="text-zinc-500">{title}</p>
+          <p className="w-[120px]">{match}</p>
+          <p className="text-gray-600 truncate">{title}</p>
         </div>
         <p>{date}</p>
       </div>
--- src/features/landing/ui/kakao-login-button.tsx
+++ src/features/landing/ui/kakao-login-button.tsx
@@ -3,15 +3,8 @@ import Image from "next/image";
 export default function KakaoLoginButton() {
   return (
     <button className="w-[320px] h-12 bg-[#FEE500] rounded-lg flex items-center justify-center gap-3 hover:brightness-95 transition cursor-pointer">
-      <Image
-        src="/icons/kakao_icon.svg"
-        alt="카카오 아이콘"
-        width={24}
-        height={24}
-        className="w-6 h-6"
-        priority
-      />
-      <span className="text-[15px] font-medium text-[#191600]">카카오톡으로 로그인</span>
+      <img src="/icons/kakao_icon.svg" alt="카카오 아이콘" className="w-6 h-6" />
+      <span className="b01-m text-black">카카오톡으로 로그인하기</span>
     </button>
   );
 }

222041a12dcbf473b86285d766243ba8a4f3c485
psm1st <psm1st@sookmyung.ac.kr>
2025-10-06 03:08:48 +0900

    chore : 추가적인 디자인 시스템 등록

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,17 +3,11 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: [
     "../src/**/storybook/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/storybook/**/*.mdx",
-
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/*.mdx",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {
-      builder: "vite",
-    },
-  },
+  framework: { name: "@storybook/nextjs", options: {} }, // Vite는 nextjs 프레임워크가 알아서 씀
+  staticDirs: ["../public"], // 상대경로만 쓰면 OK
 };
 export default config;

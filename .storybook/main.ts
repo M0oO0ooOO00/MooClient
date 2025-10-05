9647892c39682119d4f8307434355e4b2a655768
psm1st <psm1st@sookmyung.ac.kr>
2025-10-06 00:23:31 +0900

    fix : 스토리북 빌드 실패 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,13 +8,7 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/*.mdx",
   ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: {
     name: "@storybook/nextjs",
     options: {

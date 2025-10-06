ac12a6438552111b05c9a385af22f66e4c4c46a2
HoyeongJeon <hoyeong0302@gmail.com>
2025-10-06 18:12:22 +0900

    fix: [#21] 코드 리팩토링 - svg 파일 public으로 분리 후 import 사용 - padding 같은 간격은 p 하나로 통일 - 디자인시스템에 있는 색상은 디자인 시스템에서 가져오기

--- src/features/my/ui/My.tsx
+++ src/features/my/ui/My.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function My() {
   return (
     <>
-      <div className="bg-[#ECECEE] min-h-screen w-full flex flex-col items-center px-40 pt-15 pb-20">
+      <div className="bg-gray-100 min-h-screen w-full flex flex-col items-center px-40 pt-15 pb-20">
         <h1 className="text-t01 font-bold w-full pb-15">마이페이지</h1>
-        <div className="flex flex-col rounded-[20px] border pt-[30px] pr-[30px] pb-[30px] pl-[30px] gap-[10px] bg-white shadow-md w-full mb-10">
+        <div className="flex flex-col rounded-[20px] border p-[30px] gap-[10px] bg-white shadow-md w-full mb-10">
           <div className="flex justify-between mt-0 mb-4 border-b">
             <div className="flex items-center gap-4 mb-6">
               {/* 프로필 이미지 */}
@@ -70,45 +70,20 @@ export default function My() {
         </div>
         <div className="flex flex-col rounded-[20px] border py-[30px] px-[6px] gap-[10px] bg-white shadow-md w-full mb-10">
           {/* 콘텐츠 */}
-          <div className="flex flex-row gap-2 py-[16px] px-[30px] hover:bg-gray-50 cursor-pointer">
-            <svg
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.1523 24.5227L10.8121 24.1895L11.1523 24.5227ZM15.9157 21.049L16.2559 20.7158L15.9157 21.049ZM14.555 21.049L14.2148 20.7158L14.555 21.049ZM20.9512 6.66527H20.475V23.8562H20.9512H21.4274V6.66527H20.9512ZM19.3184 24.5227L19.6586 24.1895L16.2559 20.7158L15.9157 21.049L15.5755 21.3822L18.9783 24.8559L19.3184 24.5227ZM14.555 21.049L14.2148 20.7158L10.8121 24.1895L11.1523 24.5227L11.4924 24.8559L14.8952 21.3822L14.555 21.049ZM9.51953 23.8562H9.99572V6.66527H9.51953H9.04334V23.8562H9.51953ZM10.4719 5.71289V6.18908H19.9988V5.71289V5.2367H10.4719V5.71289ZM9.51953 6.66527H9.99572C9.99572 6.40228 10.2089 6.18908 10.4719 6.18908V5.71289V5.2367C9.68293 5.2367 9.04334 5.87629 9.04334 6.66527H9.51953ZM11.1523 24.5227L10.8121 24.1895C10.5136 24.4942 9.99572 24.2829 9.99572 23.8562H9.51953H9.04334C9.04334 25.1361 10.5968 25.7702 11.4924 24.8559L11.1523 24.5227ZM15.9157 21.049L16.2559 20.7158C15.6957 20.1439 14.775 20.1439 14.2148 20.7158L14.555 21.049L14.8952 21.3822C15.0819 21.1916 15.3888 21.1916 15.5755 21.3822L15.9157 21.049ZM20.9512 23.8562H20.475C20.475 24.2829 19.9572 24.4942 19.6586 24.1895L19.3184 24.5227L18.9783 24.8559C19.8739 25.7702 21.4274 25.1361 21.4274 23.8562H20.9512ZM20.9512 6.66527H21.4274C21.4274 5.87629 20.7878 5.2367 19.9988 5.2367V5.71289V6.18908C20.2618 6.18908 20.475 6.40228 20.475 6.66527H20.9512Z"
-                fill="#6F7176"
-              />
-            </svg>
+          <div className="flex flex-row gap-3 py-[4px] px-[30px] hover:bg-gray-50 cursor-pointer">
+            <Image src="/icons/bookmark.svg" alt="스크랩 한 모집글" width={30} height={30} />
             <p className="font-weight-m text-b01 leading-150">스크랩 한 모집글</p>
           </div>
-          <div className="flex flex-row gap-2 py-[16px] px-[30px] hover:bg-gray-50 cursor-pointer">
-            <svg
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M23.577 9.04186L20.5591 6.02396C20.1252 5.59004 19.4217 5.59004 18.9878 6.02396L9.36531 15.6464C9.20764 15.8041 9.09362 16 9.03441 16.215L7.74013 20.9143C7.45442 21.9517 8.49169 22.8611 9.4828 22.4422L13.6073 20.6989C13.7602 20.6342 13.8991 20.5407 14.0165 20.4233L23.577 10.8628C24.0799 10.36 24.0799 9.5447 23.577 9.04186Z"
-                stroke="#6F7176"
-                stroke-width="0.952381"
-                stroke-linecap="round"
-              />
-            </svg>
+          <div className="flex flex-row  gap-3 py-[4px] px-[30px] hover:bg-gray-50 cursor-pointer">
+            <Image src="/icons/pen.svg" alt="내가 작성한 모집글" width={30} height={30} />
             <p className="font-weight-m text-b01 leading-150">내가 작성한 모집글</p>
           </div>
-          <div className="flex flex-row gap-2 py-[16px] px-[30px] hover:bg-gray-50 cursor-pointer">
+          <div className="flex flex-row  gap-3 py-[4px] px-[30px] hover:bg-gray-50 cursor-pointer">
             <Image
               src="/images/default.png"
               alt="내가 참여한 글"
               width={30}
               height={30}
-              className="cursor-pointer"
               // onClick={() => {/* 클릭 핸들러 */}}
             />
             <p className="font-weight-m text-b01 leading-150">내가 참여한 글</p>

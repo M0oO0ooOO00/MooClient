2b2977040ef1c67599ead3b3fe8322d26b202e74
Choi Yong Won <cywin0821@gmail.com>
2025-10-06 23:04:38 +0900

    fix-api

--- src/features/landing/ui/kakao-login-button.tsx
+++ src/features/landing/ui/kakao-login-button.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function KakaoLoginButton() {
   return (
     <button className="w-[320px] h-12 bg-[#FEE500] rounded-lg flex items-center justify-center gap-3 hover:brightness-95 transition cursor-pointer">
-      <img src="/icons/kakao_icon.svg" alt="카카오 아이콘" className="w-6 h-6" />
+      <Image src="/icons/kakao_icon.svg" alt="카카오 아이콘" width={24} height={24} />
       <span className="text-b01-m text-black">카카오톡으로 로그인하기</span>
     </button>
   );

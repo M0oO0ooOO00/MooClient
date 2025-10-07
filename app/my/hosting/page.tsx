af3fa450bb84d75d55b2c5d4a660608ebb3c03c6
HoyeongJeon <hoyeong0302@gmail.com>
2025-10-07 11:49:18 +0900

    design: [#36] 내가 작성한 모집 글 UI 구현

---
+++ app/my/hosting/page.tsx
@@ -0,0 +1,9 @@
+import Hosting from "@/features/my/ui/Hosting";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-[56px]">
+      <Hosting />
+    </div>
+  );
+}

54481149b5cf6aa44d30dd29aa407a1b7fd10014
HoyeongJeon <hoyeong0302@gmail.com>
2025-10-07 11:49:00 +0900

    design: [#36] 내가 스크랩 한 글 UI 구현

---
+++ app/my/scrap/page.tsx
@@ -0,0 +1,9 @@
+import MyScrap from "@/features/my/ui/MyScrap";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-[56px]">
+      <MyScrap />
+    </div>
+  );
+}
